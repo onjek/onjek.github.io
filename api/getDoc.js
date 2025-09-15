8a41829fc5e5755896822a488c5a3ea911473980
1010 <134185805+jedenzero@users.noreply.github.com>
2025-09-15 17:36:53 +0900

    getDoc.js 업데이트

--- api/getDoc.js
+++ api/getDoc.js
@@ -1,23 +1,24 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
     const { id } = req.query;
     
     if (!id) {
-        res.status(400).json({ "오류: id 필요" });
+        res.status(400).json({ error: "id 필요" });
         return;
     }
-    
     const url = `https://raw.githubusercontent.com/onjek/data/main/docs/${id}.md`;
     
     try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error("fetch 실패");
-        
-        const text = await response.text();
-        
-        res.status(200).send(text);
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        res.status(response.status).json({ error: `GitHub fetch 실패: ${response.statusText}` });
+        return;
+    }
+    const text = await response.text();
+    
+    res.status(200).send(text);
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        console.error(err);
+        res.status(500).json({ error: "서버 오류: " + err.message });
     }
 }
