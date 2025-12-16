49edc18f4a1f9c7faa2d3bed9475301dd6238990
1010 <134185805+jedenzero@users.noreply.github.com>
2025-12-16 22:23:54 +0900

    App.js 업데이트

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const md = new MarkdownIt({ html: true })
     .use(markdownItMultimdTable, { headerless: true, rowspan: true })
     .use(markdownItTh)
     .use(markdownItAnchor)
-    .use(markdownItTOC, { placeholder: '((toc))' })
+    .use(markdownItTOC, { placeholder: '(\(\(toc\)\))' })
     .use(markdownItContainer, 'info', {
         render: function(tokens, idx){
             const token = tokens[idx];
